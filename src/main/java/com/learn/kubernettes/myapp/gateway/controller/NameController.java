47c764f25fbb3998b7c4e67993baec1f9bc8f753
System Administrator <root@ameerah.local>
2019-04-26 14:14:52 +0200

    deployment, service, namespace,ingress

--- src/main/java/com/learn/kubernettes/myapp/gateway/controller/NameController.java
+++ src/main/java/com/learn/kubernettes/myapp/gateway/controller/NameController.java
@@ -1,5 +1,6 @@
 package com.learn.kubernettes.myapp.gateway.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,12 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Random;
 
 @RestController
+@Slf4j
 public class NameController {
 
     @GetMapping("/name")
     public ResponseEntity<String> getRandomName() {
         String name [] = {"Ameerah", "Yaqub"};
+        String selectedName = name[new Random().nextInt(2)];
+        log.info("The selected name is {}", selectedName);
 
-        return ResponseEntity.ok(name[new Random().nextInt(2)]);
+        return ResponseEntity.ok(selectedName);
     }
 }
